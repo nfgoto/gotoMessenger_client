1718a106c5679440db6c8e40d30341eb9cec172e
nfgoto <gotoflorian.pro@gmail.com>
2019-02-08 15:33:53 +0100

    update react-scripts to be compatible wirh typescrypt handling out of the box

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -192,13 +192,13 @@ class Feed extends Component {
         return res.json();
       })
       .then(resData => {
-        const post = {
-          _id: resData.post._id,
-          title: resData.post.title,
-          content: resData.post.content,
-          creator: resData.post.creator,
-          createdAt: resData.post.createdAt
-        };
+        // const post = {
+        //   _id: resData.post._id,
+        //   title: resData.post.title,
+        //   content: resData.post.content,
+        //   creator: resData.post.creator,
+        //   createdAt: resData.post.createdAt
+        // };
         this.setState(prevState => {
           return {
             isEditing: false,
